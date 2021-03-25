43dfbfd5208883ae0f28b7183a80c36d6622cbfc
Faye Zhang <fzhang@eastsideprep.org>
2021-03-24 18:29:22 -0700

    Secret Biology Project
    shhhh

---
+++ secret/reaction/reaction-style.js
@@ -0,0 +1,67 @@
+function getRandomColor() {
+				
+    var letters = "0123456789ABCDEF".split('');
+    var color = "#";
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.round(Math.random() * 15)];
+    } //ends for loop 
+    return color; 
+
+
+} // ends getRandomColor Function
+
+
+var clickedTime; var createdTime; var reactionTime; 
+
+function makeBox() {
+        var time=Math.random();
+        time=time*4000;
+    
+    setTimeout(function() {
+    
+        if (Math.random()>0.5) {
+        
+            document.getElementById("box").style.borderRadius="100px";
+            
+            } else {
+            
+                document.getElementById("box").style.borderRadius="0";
+            }
+            
+        var top= 120;
+        var left= 150;
+            
+        document.getElementById("box").style.top = top + "px";
+        document.getElementById("box").style.left = left + "px"; 
+    
+        document.getElementById("box").style.backgroundColor=getRandomColor();
+    
+        document.getElementById("box").style.display="block";
+        
+        createdTime=Date.now();
+        
+    }, time); 
+
+}
+
+document.getElementById("box").onclick=function() {
+
+    clickedTime=Date.now();
+    
+    reactionTime=(clickedTime-createdTime)/1000;
+    
+    document.getElementById("printReactionTime").innerHTML="Your Reaction Time is: " + reactionTime + "seconds";
+    
+    this.style.display="none";
+    
+    makeBox();
+    
+    
+}
+
+makeBox(); 
+
+
+
+
+
